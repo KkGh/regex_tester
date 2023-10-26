8881d08df6db3d20cd65f98148434c9d6da4e286
kos <geneofgene@gmail.com>
2023-10-26 19:43:54 +0900

    add: github button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
+import { FaGithub } from "react-icons/fa";
 import "./App.css";
 import { TESTINPUT, TESTREG } from "./constants";
 import { MainContent } from "./pages/main/MainContent";
@@ -7,8 +8,17 @@ import { MainContent } from "./pages/main/MainContent";
 const App = () => {
   return (
     <div>
-      <Navbar className="header">
-        <Navbar.Brand href="/">Regex Tester</Navbar.Brand>
+      <Navbar className="header px-3" style={{ height: "56px" }}>
+        <Navbar.Brand href="./">Regex Tester</Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/KkGh/regex_tester"
+          >
+            <FaGithub size="24px" />
+          </Nav.Link>
+        </Nav>
       </Navbar>
 
       <main className="py-3">
